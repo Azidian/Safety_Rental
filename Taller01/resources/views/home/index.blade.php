a530ed9f94afd0eff558e37f0e64f29ba15a660d
Isabellaa <isabellaocampo128@gmail.com>
2026-08-17 00:13:37 -0500

    Delete Taller01 directory

--- Taller01/resources/views/home/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app') 
-@section('title', 'Home Page - Safety Rental') 
-@section('content') 
-<div class="d-flex flex-column justify-content-center align-items-center text-center w-100" style="min-height: 60vh;">
-    
-    <img src="{{ asset('img/Safety_Rental_Banner.png') }}" alt="Safety Rental Car" class="img-fluid" style="max-width: 80%;">
-    
-</div>
-@endsection 
